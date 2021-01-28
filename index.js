21c3a4269f26c27ec2b5e6822f6916b67043abb3
samplesn08 <samplesn08@gmail.com>
2021-01-28 11:40:22 -0500

    task 1 passing

--- index.js
+++ index.js
@@ -42,7 +42,24 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+    eat(someFood) {
+      if (this.stomach.length < 10) {
+        this.stomach.push(someFood);
+      }else {
+        this.stomach.push(null);
+      }
+    }
+    poop() {
+      this.stomach = [];
+    }
+    toString() {
+      return `${this.name}, ${this.age}`;
+    }
   }
   
   /*
